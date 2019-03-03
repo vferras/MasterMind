7d86ba065a06f853bedba22bb6470b9a872e2c26
Víctor Ferràs Gómez <vferras@MacBook-Pro-de-Victor-2.local>
2019-03-03 20:00:33 +0100

    Add swagger support

--- README.md
+++ README.md
@@ -26,3 +26,7 @@ Once the script is successfuly executed, you can reach the api through the url: 
 To run the tests manually, you should have dotnet core 2.2 installed, and then navigate to the test folder and run the following command: dotnet test
 
 To run the app manually, please, use the command: dotnet run
+
+**Swagger**
+
+To run swagger, please, run the app and then navigate to http://localhost:8080/swagger
--- src/Api/Startup.cs
+++ src/Api/Startup.cs
@@ -4,6 +4,7 @@ using Microsoft.Extensions.DependencyInjection;
 using Newtonsoft.Json;
 using Newtonsoft.Json.Converters;
 using Newtonsoft.Json.Serialization;
+using Swashbuckle.AspNetCore.Swagger;
 
 namespace Api
 {
@@ -15,6 +16,7 @@ namespace Api
         {
             services
                 .AddMvcCore()
+                .AddApiExplorer()
                 .AddJsonFormatters(settings =>
                 {
                     settings.ContractResolver = new DefaultContractResolver();
@@ -24,12 +26,20 @@ namespace Api
                     settings.PreserveReferencesHandling = PreserveReferencesHandling.None;
                     settings.Converters.Add(new StringEnumConverter());
                 });
+
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new Info { Title = "Payments International API", Version = "v1" });
+            });
         }
 
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IHostingEnvironment env)
         {
-            app.UseMvc();
+            app
+                .UseSwagger()
+                .UseSwaggerUI(c => { c.SwaggerEndpoint("/swagger/v1/swagger.json", "Payments API"); })
+                .UseMvc();
         }
     }
 }
